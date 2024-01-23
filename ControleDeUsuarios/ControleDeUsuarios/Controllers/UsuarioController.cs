58e73c0f2bda42789989567ac008281a62750f2b
kauemelo <kauemelo01@gmail.com>
2024-01-23 19:21:04 -0300

    Adicionado 2 Metodos

---
+++ ControleDeUsuarios/ControleDeUsuarios/Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeUsuarios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ola")]
+        public IActionResult OlaPessoa()
+        {
+            string nome = "Jose";
+            if (nome.Equals("Maria"))
+            {
+                return Ok($"Olá {nome}");
+            }
+            else
+            {
+                return NotFound();
+            }
+           
+        }
+        [HttpGet("teste/outroteste")]
+        public IActionResult Teste ()
+        {
+            return Ok("Teste");
+        }
+    }
+}
